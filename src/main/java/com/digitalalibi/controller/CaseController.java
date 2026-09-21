852b8eb05a6cb722f80ccbabedf5788550307cf3
Agrima0408 <agrima.ag04@gmail.com>
2026-09-21 20:17:33 +0530

    updated CaseController

--- src/main/java/com/digitalalibi/controller/CaseController.java
+++ src/main/java/com/digitalalibi/controller/CaseController.java
@@ -1,8 +1,12 @@
 package com.digitalalibi.controller;
 
+import com.digitalalibi.entity.Case;
 import com.digitalalibi.service.CaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CaseController {
@@ -10,6 +14,20 @@ public class CaseController {
     @Autowired
     public void setCaseService(CaseService caseService) {
         this.caseService = caseService;
+    }
+    @GetMapping("/api/cases")
+        public List<Case> getAllCases(){
+            return caseService.findAll();
+        }
+
+    @GetMapping("/api/cases/{id}")
+    public Optional<Case> getCase(@PathVariable Long id){
+        return caseService.findById(id);
+    }
+
+    @PostMapping("/api/cases")
+    public Case createCase(@RequestBody Case c){
+        return caseService.save(c);
     }
 }
 
